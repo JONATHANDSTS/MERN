e765a9c4559e50741dd36c3a653cf239da2be4ea
JONATHANDSTS <jonath850@gmail.com>
2021-02-22 18:45:08 +0100

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+//variables d'environnement
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+//express
+const express = require("express");
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`listenning on ${PORT}`);
+});
