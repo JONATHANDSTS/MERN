24c800bc32f5f088bebac9d21fac193d11746af7
JONATHANDSTS <jonath850@gmail.com>
2021-02-23 14:21:04 +0100

    encrypt password

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 //on appelle isEmail de la lib validator pour la valisation de l'email
 const { isEmail } = require("validator");
+//pour crypter le password on importe la lib
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -25,9 +27,9 @@ const userSchema = new mongoose.Schema(
       max: 1024,
       minLenght: 6,
     },
-    pictures:{
-        type:String,
-        default:"./upload/profil/random-user.png"
+    pictures: {
+      type: String,
+      default: "./upload/profil/random-user.png",
     },
     bio: {
       type: String,
@@ -49,5 +51,16 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-const UserModel = mongoose.model('user', userSchema);
+// play function before save into display "block"\
+// pas de fonction fleche car on utilise .this
+userSchema.pre("save", async function (next) {
+  // bcrypt va generer une serie de characteres que lui seul connait pour sale le password
+  const salt = await bcrypt.genSalt();
+  //une fois sale, on doit ajouter a notre password, donc .this
+  this.password = await bcrypt.hash(this.password, salt);
+  // cest un middleware donc il faut finir par next()
+  next();
+});
+
+const UserModel = mongoose.model("user", userSchema);
 module.exports = UserModel;
