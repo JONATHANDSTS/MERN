655a11e86faf914b1ade6accd1523ba93ceaa6c6
JONATHANDSTS <jonath850@gmail.com>
2021-02-26 10:21:19 +0100

    crud comments

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -3,10 +3,13 @@ const userModel = require("../models/user.model");
 const ObjectId = require("mongoose").Types.ObjectId;
 
 module.exports.readPost = (req, res) => {
-  postModel.find((err, docs) => {
-    if (!err) res.send(docs);
-    else console.log("Error to get data :" + err);
-  });
+  postModel
+    .find((err, docs) => {
+      if (!err) res.send(docs);
+      else console.log("Error to get data :" + err);
+      // on trie dans l'odre du post du plus recent au plus ancien avec .sort
+    })
+    .sort({ createdAt: -1 });
 };
 module.exports.createPost = async (req, res) => {
   const newPost = new postModel({
@@ -103,6 +106,81 @@ module.exports.unlikePost = async (req, res) => {
       req.body.id,
       {
         $pull: { likes: req.params.id },
+      },
+      { new: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        else return res.status(400).send(err);
+      }
+    );
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
+
+module.exports.commentPost = (req, res) => {
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(400).send("ID unknown" + req.params.id);
+  try {
+    return postModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: {
+          comments: {
+            commentId: req.body.commenterId,
+            commenterPseudo: req.body.commenterPseudo,
+            text: req.body.text,
+            timestamp: new Date().getTime(),
+          },
+        },
+      },
+      { new: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        else return res.status(400).send(err);
+      }
+    );
+  } catch (err) {
+    return res.status(400).send(err + "ici");
+  }
+};
+
+module.exports.editCommentPost = (req, res) => {
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(400).send("ID unknown" + req.params.id);
+
+  try {
+    return postModel.findById(req.params.id, (err, docs) => {
+      const theComment = docs.comments.find((comment) =>
+        comment._id.equals(req.body.commentId)
+      );
+
+      if (!theComment) return res.status(400).send("comment not found");
+      theComment.text = req.body.text;
+
+      return docs.save((err) => {
+        if (!err) return res.status(200).send(docs);
+        return res.status(400).send(err);
+      });
+    });
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
+
+module.exports.deleteCommentPost = (req, res) => {
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(400).send("ID unknown" + req.params.id);
+
+  try {
+    return postModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $pull: {
+          comments: {
+            _id: req.body.commentId,
+          },
+        },
       },
       { new: true },
       (err, docs) => {
